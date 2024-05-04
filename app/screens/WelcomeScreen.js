a497228e515858836530178f420a9e9c683b022e
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2024-05-04 08:41:40 -0400

    welcome screen styled

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -11,38 +11,39 @@ import colors from "../config/colors";
 
 export default function Welcome() {
 	return (
-		<View style={styles.container}>
-			<ImageBackground
-				style={{ width: "100%", height: "100%" }}
+		<>
+			<Image
+				style={{
+					position: "absolute",
+					zIndex: "-1",
+					width: "100%",
+					height: "100%",
+				}}
 				source={require("../assets/background.jpg")}
-			>
-				<SafeAreaView
-					style={{
-						width: "100%",
-						flex: 1,
-						padding: 400,
-						alignItems: "center",
-						flexDirection: "column",
-					}}
-				>
-					<Image
-						style={{ marginTop: 40, width: 100, height: 100 }}
-						source={require("../assets/logo-red.png")}
-					/>
-					<Text style={{ marginTop: 10 }}>Sell what you don't need.</Text>
-					<AppButton title="Login" />
-					<AppButton title="Sign Up" color="secondary" />
-				</SafeAreaView>
-			</ImageBackground>
-		</View>
+			/>
+			<SafeAreaView style={styles.container}>
+				<Image
+					style={{ position: "absolute", top: 100, width: 100, height: 100 }}
+					source={require("../assets/logo-red.png")}
+				/>
+				<Text style={{ position: "absolute", top: 200, marginTop: 10 }}>
+					Sell what you don't need.
+				</Text>
+				<AppButton title="Login" />
+				<AppButton title="Sign Up" color="secondary" />
+			</SafeAreaView>
+		</>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
+		padding: 10,
 		flex: 1,
+		height: "100%",
+		flexDirection: "column",
 		width: "100%",
-		alignItems: "flex-end",
+		alignItems: "center",
 		justifyContent: "flex-end",
 	},
 });
